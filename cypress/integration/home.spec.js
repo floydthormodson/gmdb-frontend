1f6a1d917d0f41d6414fe11aacdd2008a3951479
floydthormodson <floydthormodson@outlook.com>
2021-10-21 15:30:37 -0400

    Adds passings cypress test that detects the title of the page.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,3 +4,13 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+
+describe("Home page", () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+    it("the home page ocontains a title:", () => {
+      cy.findByRole('heading').should('contain', 'GMDB')
+    })
+  })
